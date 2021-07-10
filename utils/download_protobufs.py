a5120d58edabfce36e0f4511e4e0db4e004f607e
Jonathan Stewmon <jstewmon@gmail.com>
2021-07-10 12:10:04 -0500

    wip: rewrite proto downloader to be more declarative

--- utils/download_protobufs.py
+++ utils/download_protobufs.py
@@ -1,122 +1,149 @@
+import re
 import os
 import zipfile
+
 import requests
 import structlog
+
 from copy import deepcopy
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Sequence, Optional
+
 from grpc_tools import protoc
 
-utf8 = 'utf-8'
-
-proto_include = protoc.pkg_resources.resource_filename('grpc_tools', '_proto')
+utf8 = "utf-8"
 
 structlog.configure()
 logger = structlog.get_logger()
 
-archives_to_unpack = (
-    ('envoy', 'https://github.com/envoyproxy/data-plane-api/archive/master.zip', 'data-plane-api'),
-    ('google', 'https://github.com/googleapis/googleapis/archive/master.zip', 'googleapis'),
-    ('udpa', 'https://github.com/cncf/udpa/archive/master.zip', 'udpa'),
-    ('validate', 'https://github.com/envoyproxy/protoc-gen-validate/archive/master.zip', 'protoc-gen-validate'),
-    ('protobuf', 'https://github.com/protocolbuffers/protobuf/archive/master.zip', 'protobuf'),
+
+@dataclass(frozen=True)
+class Source:
+    """
+    Class representing a source archive to be downloaded and extracted.
+
+    By default, files are extracted to a directory named `name`. An alternate
+    directory can be specified with `output`.
+
+    The root folder of a source archive is always discarded, such that `root` is
+    equivalent to the repository root.
+
+    If an alternate `root` is specified, `pattern` is evaluated against
+    filenames relative to root. Extracted filenames are relative to `root`. i.e.
+    the `root` portion of filename is discarded during extraction.
+
+    Attributes
+    ----------
+    name (str): name of the source, used as `output` by default
+    url (str): URL for the archive
+    pattern (Pattern): regex pattern used to filter archive contents
+    root (Path): directory in the archive to treat as root
+    output (Path): directory to which files are extracted (defaults to `name`)
+    """
+
+    name: str
+    url: str
+    pattern: re.Pattern = re.compile(r"^.*\.proto$")
+    root: Path = Path("")
+    output: Optional[Path] = None
+
+
+sources: Sequence[Source] = (
+    Source(
+        "envoy",
+        "https://github.com/envoyproxy/data-plane-api/archive/master.zip",
+        root=Path("envoy"),
+    ),
+    Source(
+        "google",
+        "https://github.com/googleapis/googleapis/archive/master.zip",
+        pattern=re.compile(r"^(api|rpc)/.*\.proto$"),
+        root=Path("google"),
+    ),
+    Source(
+        "udpa", "https://github.com/cncf/udpa/archive/master.zip", root=Path("udpa")
+    ),
+    Source(
+        "validate",
+        "https://github.com/envoyproxy/protoc-gen-validate/archive/master.zip",
+        root=Path("validate"),
+    ),
+    Source(
+        "protobuf",
+        "https://github.com/protocolbuffers/protobuf/archive/master.zip",
+        root=Path("src/google/protobuf"),
+        output=Path("google/protobuf"),
+    ),
+    Source(
+        "opencensus",
+        "https://github.com/census-instrumentation/opencensus-proto/archive/master.zip",
+        root=Path("src/opencensus"),
+    ),
+    Source(
+        "prometheus", "https://github.com/prometheus/client_model/archive/master.zip", output=Path(".")
+    ),
 )
 
-build_directory = Path('BUILD')
+# TODO build dir context mgr
+build_directory = Path("BUILD")
 if not build_directory.exists():
-    logger.msg('Creating BUILD directory')
+    logger.msg("Creating BUILD directory")
     build_directory.mkdir()
 os.chdir(build_directory)
 
-protobuf_files = Path('github_files')
+for source in sources:
+    namespace = Path(source.name)
+    archive = Path(f"{source.name}.zip")
 
-for name, repo_url, repo_name in archives_to_unpack:
-    namespace = Path(name)
-    archive = Path(f'{repo_name}.zip')
-    extracted_name = f'{repo_name}-master'
-
+    # TODO: def get_archive(url, out_file)
     if not archive.exists():
-        logger.msg(f'Downloading {name} protocol buffers from Github')
-        with open(archive, 'wb+') as zf:
-            zf.write(requests.get(repo_url).content)
+        logger.msg(f"Downloading {source.name} protocol buffers from Github")
+        with open(archive, "wb+") as zf:
+            zf.write(requests.get(source.url).content)
 
-    if str(namespace) == 'protobuf' and Path('google').joinpath(namespace).exists():
-        continue
-
-    if not namespace.exists():
-        logger.msg(f'Extracting {namespace} archive')
-        with zipfile.ZipFile(archive, 'r') as zipref:
-            protos = [
-                member
-                for member in zipref.namelist()
-                if member.endswith('.proto')
-            ]
-            for file in protos:
-                logger.msg(f'Extracting {file[len(extracted_name) + 1:]}')
-                zipref.extract(
-                    member=file,
-                    path=protobuf_files
-                )
-
-        extracted_files_root = protobuf_files.joinpath(Path(extracted_name))
-        if str(namespace) == 'protobuf':
-            protocol_buffers = extracted_files_root.joinpath(Path(f'src/google/{namespace}'))
-            google_protos = Path(f'./google/{namespace}')
-            protocol_buffers.replace(google_protos)
-            logger.msg(f'Moving {protocol_buffers} -> {google_protos.absolute()}')
-        else:
-            protocol_buffers = extracted_files_root.joinpath(namespace)
-            protocol_buffers.replace(namespace)
-            logger.msg(f'Moving {protocol_buffers} -> {namespace.absolute()}')
+    source_root_len = len(source.root.parts)
+    # TODO: def extract_archive(in_file, out_dir)
+    if True or not namespace.exists():
+        output = source.output or Path(source.name)
+        logger.msg(f"Extracting {namespace} archive")
+        with zipfile.ZipFile(archive, "r") as zipref:
+            for info in zipref.infolist():
+                # TODO: def filter_archive(zipref, pattern, root)
+                info_path = Path(info.filename)
+                info_parts = info_path.parts
+                # strip the repo archive name e.g. {repo}-master
+                local_parts = info_parts[1:]
+                if local_parts[0:source_root_len] != source.root.parts:
+                    # skip files outside root
+                    continue
+                # TODO: def rewrite_archive_filename(info, root, out_dir)
+                norm_local_path = Path().joinpath(*local_parts[source_root_len:])
+                if not source.pattern.fullmatch(str(norm_local_path)):
+                    # skip files not matching pattern
+                    continue
+                info.filename = str(output / norm_local_path)
+                zipref.extract(info)
     else:
-        logger.msg(f'{namespace.absolute()} exists')
+        logger.msg(f"{namespace.absolute()} exists")
 
-output = Path('../src/envoy_data_plane')
-envoy = Path('./envoy')
-envoy_api = Path('./envoy/api')
-envoy_api_v2 = Path('./envoy/api/v2')
-proto_args = [
-    __file__,
-    f'--proto_path=.',
-    f'--proto_path={envoy}',
-    f'--proto_path={envoy_api}',
-    f'--proto_path={envoy_api_v2}',
-    f'--proto_path={proto_include}',
-]
+output = Path("../src/envoy_data_plane")
+# TODO: betterproto doesn't work well with Any; no Pack() method
+proto_args = ["grpc_tools.protoc", f"--python_betterproto_out={output}"]
 
 
 def everything():
-    proto_files = [
-        str(file) for file in envoy.glob('**/*')
-        if file.suffix == '.proto'
-    ]
-    proto_paths = [
-        f'--proto_path={folder}'
-        for folder in Path('.').glob('**/*')
-        if folder.is_dir()
-    ]
     args = deepcopy(proto_args)
-    args += proto_paths
-    args += [f'--python_betterproto_out={output}']
-    logger.msg('Running GRPC tools to generate python code from protocol buffers')
-    files = list()
-    for f in proto_files:
-        files.append(f)
-        if len(files) >= 100:
-            protoc.main(args + files)
-            files.clear()
-    protoc.main(args + files)
+    logger.msg("Running GRPC tools to generate python code from protocol buffers")
+    # TODO: itertools chunk see: https://www.in-ulm.de/~mascheck/various/argmax/
+    protoc.main((*args,  *map(str, Path("envoy").glob("**/*.proto"))))
 
 
 def xds():
-    proto_paths = [
-        str(envoy_api_v2.joinpath(Path(f'{discovery_type}.proto')))
-        for discovery_type in ['cds', 'lds', 'rds', 'eds', 'srds']
-    ]
-    args = deepcopy(proto_args)
-    args += proto_paths
-    args += [f'--python_betterproto_out={output}']
-    protoc.main(args)
+    v2_protos = Path("envoy/api/v2").glob("*ds.proto")
+    v3_protos = Path("envoy/service").glob("**/v3/*ds.proto")
+    protoc.main([*proto_args, *map(str, (*v2_protos, *v3_protos))])
 
 
 everything()
-xds()
+# xds()
